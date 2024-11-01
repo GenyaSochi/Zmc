eab48031749314c923b3767729a9720910f1530d
GenyaSochi <eunasedkina@gmail.com>
2024-11-01 21:29:01 +0300

    _

--- components/MainPage/MainSection.vue
+++ components/MainPage/MainSection.vue
@@ -1,45 +1,56 @@
 <template>
-
   <section style=" background: url(/img/silver1.webp); color: black;">
     <div class="container">
-      <h1 style="padding: 50px 0; text-align: center;">17 лет производим опоры для трубопроводов</h1>
-      <strong>Наличие собственного конструкторского бюро. Изготовление опор по чертежам заказчика. <br> Более 100 видов
-        продукции.</strong>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium in reprehenderit maxime exercitationem amet
-      praesentium totam voluptate magni repellat voluptates, porro fugiat impedit, quae eveniet sunt atque ab
-      repellendus et.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto quod harum dignissimos distinctio aliquam
-      expedita, optio suscipit necessitatibus saepe soluta dicta ducimus, aspernatur voluptates dolor amet. Magni, dicta
-      voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel possimus fugiat voluptatibus error unde eum
-      fuga quam nisi, id, quo minus impedit, consequuntur obcaecati blanditiis deserunt vero? Aspernatur, saepe.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero voluptatibus facilis corrupti, asperiores odit
-      explicabo sed cupiditate rem alias dolorem veritatis doloremque molestias, natus recusandae totam error nulla
-      doloribus! Reiciendis?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium in reprehenderit maxime exercitationem amet
-      praesentium totam voluptate magni repellat voluptates, porro fugiat impedit, quae eveniet sunt atque ab
-      repellendus et.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto quod harum dignissimos distinctio aliquam
-      expedita, optio suscipit necessitatibus saepe soluta dicta ducimus, aspernatur voluptates dolor amet. Magni, dicta
-      voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel possimus fugiat voluptatibus error unde eum
-      fuga quam nisi, id, quo minus impedit, consequuntur obcaecati blanditiis deserunt vero? Aspernatur, saepe.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero voluptatibus facilis corrupti, asperiores odit
-      explicabo sed cupiditate rem alias dolorem veritatis doloremque molestias, natus recusandae totam error nulla
-      doloribus! Reiciendis?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium in reprehenderit maxime exercitationem amet
-      praesentium totam voluptate magni repellat voluptates, porro fugiat impedit, quae eveniet sunt atque ab
-      repellendus et.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto quod harum dignissimos distinctio aliquam
-      expedita, optio suscipit necessitatibus saepe soluta dicta ducimus, aspernatur voluptates dolor amet. Magni, dicta
-      voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel possimus fugiat voluptatibus error unde eum
-      fuga quam nisi, id, quo minus impedit, consequuntur obcaecati blanditiis deserunt vero? Aspernatur, saepe.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero voluptatibus facilis corrupti, asperiores odit
-      explicabo sed cupiditate rem alias dolorem veritatis doloremque molestias, natus recusandae totam error nulla
-      doloribus! Reiciendis?
+      <h1 style="padding: 50px 0; text-align: center;">17 лет производим опоры для трубопроводов.</h1>
+      
+      <strong> &#9745 Наличие собственного конструкторского бюро. <br>&#9745 Изготовление продукции по чертежам заказчика.</strong> <br>&#9745 Точное
+        соответствие техническим требованиям. <br>&#9745 Любые объемы и типы опор. <br>&#9745 
+        Более 100 видов продукции. <br>&#9745 
+        Продукция ЗМК Урал соответствует отраслевым стандартам и нормам качества.
+      <br>
+      <br>  
+
+      <span style="font-size: 20px;">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod architecto commodi necessitatibus nihil, beatae
+        explicabo nobis quia incidunt veritatis, ad quae impedit velit quaerat pariatur voluptatem quisquam unde dolores!
+        Sequi.
+      </span>
 
     </div>
   </section>
-
 </template>
+
+<style scoped></style>
