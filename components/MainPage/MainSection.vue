8e2ea4248ebf11ea8c65f7fb6620ad7c9046d413
GenyaSochi <eunasedkina@gmail.com>
2024-11-02 00:33:40 +0300

    _

--- components/MainPage/MainSection.vue
+++ components/MainPage/MainSection.vue
@@ -2,42 +2,8 @@
   <section style=" background: url(/img/silver1.webp); color: black;">
     <div class="container">
       <h1 style="padding: 50px 0; text-align: center;">17 лет производим опоры для трубопроводов</h1>
-      <strong>Наличие собственного конструкторского бюро. Изготовление опор по чертежам заказчика. <br> Более 100 видов
-        продукции.</strong>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium in reprehenderit maxime exercitationem amet
-      praesentium totam voluptate magni repellat voluptates, porro fugiat impedit, quae eveniet sunt atque ab
-      repellendus et.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto quod harum dignissimos distinctio aliquam
-      expedita, optio suscipit necessitatibus saepe soluta dicta ducimus, aspernatur voluptates dolor amet. Magni, dicta
-      voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel possimus fugiat voluptatibus error unde eum
-      fuga quam nisi, id, quo minus impedit, consequuntur obcaecati blanditiis deserunt vero? Aspernatur, saepe.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero voluptatibus facilis corrupti, asperiores odit
-      explicabo sed cupiditate rem alias dolorem veritatis doloremque molestias, natus recusandae totam error nulla
-      doloribus! Reiciendis?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium in reprehenderit maxime exercitationem amet
-      praesentium totam voluptate magni repellat voluptates, porro fugiat impedit, quae eveniet sunt atque ab
-      repellendus et.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto quod harum dignissimos distinctio aliquam
-      expedita, optio suscipit necessitatibus saepe soluta dicta ducimus, aspernatur voluptates dolor amet. Magni, dicta
-      voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel possimus fugiat voluptatibus error unde eum
-      fuga quam nisi, id, quo minus impedit, consequuntur obcaecati blanditiis deserunt vero? Aspernatur, saepe.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero voluptatibus facilis corrupti, asperiores odit
-      explicabo sed cupiditate rem alias dolorem veritatis doloremque molestias, natus recusandae totam error nulla
-      doloribus! Reiciendis?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium in reprehenderit maxime exercitationem amet
-      praesentium totam voluptate magni repellat voluptates, porro fugiat impedit, quae eveniet sunt atque ab
-      repellendus et.
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto quod harum dignissimos distinctio aliquam
-      expedita, optio suscipit necessitatibus saepe soluta dicta ducimus, aspernatur voluptates dolor amet. Magni, dicta
-      voluptate!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel possimus fugiat voluptatibus error unde eum
-      fuga quam nisi, id, quo minus impedit, consequuntur obcaecati blanditiis deserunt vero? Aspernatur, saepe.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero voluptatibus facilis corrupti, asperiores odit
-      explicabo sed cupiditate rem alias dolorem veritatis doloremque molestias, natus recusandae totam error nulla
-      doloribus! Reiciendis?
-
+      <strong>Наличие собственного конструкторского бюро. Изготовление опор по чертежам заказчика.</strong>
+      <br>
     </div>
   </section>
 </template>
--- pages/main.vue
+++ pages/main.vue
@@ -1,9 +1,6 @@
 <template>
-  <main style="color: whitesmoke;">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repellat pariatur dolore dolor assumenda animi
-      soluta culpa tenetur, est eligendi porro voluptas, maxime quibusdam, fuga minima facere quis quae illo
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure debitis minima aliquid ea blanditiis distinctio unde
-      fuga expedita quia, a rerum explicabo aliquam mollitia cumque sed quae consectetur nam earum.</p>
+  <MainSection>
+    
 
-  </main>
+  </MainSection>
 </template>
