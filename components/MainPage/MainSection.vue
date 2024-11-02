4bab96ebcb38aa1c753aa583dad668830333bcd2
GenyaSochi <eunasedkina@gmail.com>
2024-11-02 21:06:42 +0300

    _

--- components/MainPage/MainSection.vue
+++ components/MainPage/MainSection.vue
@@ -2,17 +2,17 @@
   <section style=" background: url(/img/silver1.webp); color: black;">
     <div class="container">
       <h1 style="padding: 50px 0; text-align: center;">17 лет производим опоры для трубопроводов</h1>
-      <strong>Наличие собственного конструкторского отдела. <span style="color: white;">И</span>зготовление опор по чертежам заказчика.</strong>
+      <strong>Наличие собственного конструкторского отдела.Изготовление опор по чертежам заказчика.</strong>
       <br>
       <br>
       <div class="text">Количество выпущенной
         продукции <span class="num">5</span><span class="num">4</span><span class="num"
           style="color: white;">9</span><span class="num">4</span><span class="num">3</span><span
           class="num">9</span><span class="num">1</span><span class="num">2</span><span class="num">3</span>
+        </div>
       </div>
-    </div>
+    </section>
     <div>Продукция, выпускаемая заводом, соответствует отравслевым стандартам и нормам качества.</div>
-  </section>
 </template>
 
 <script>
