f865bea9fd96634f516921b19cd0e7ec8d4c816e
GenyaSochi <eunasedkina@gmail.com>
2025-03-10 23:07:43 +0300

    _

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -95,152 +95,218 @@ const toggleMenu = () => {
 }
 
 @media screen and (max-width:1920px) {
-.navtag {
-  font-size: 22px;
-}
-
-.nav {
-  font-size: 22px; 
-}
-
-.but {
-  font-size: 22px;
-}
-
-
-.butnav {  
-  font-size: 22px;
-}
-}
-
-@media screen and (max-width:1398px) {
-.navtag {
-  font-size: 20px;
-}
-
-.nav {
-  font-size: 20px; 
-}
-
-.but {
-  font-size: 20px;
-}
-
-.butnav {
-  font-size: 20px;
-}
-}
-
-@media screen and (max-width:1358px) {
-.navtag { 
-  font-size: 20px;
-}
-
-.nav {
-  font-size: 20px;  
-}
-
-.but {
-  font-size: 20px;  
-  gap: 20px;
-}
-
-.link {
-  display: flex;
-  gap: 38px;
-}
-
-.butnav {
-  width: 260px;  
-  font-size: 20px;
-}
-}
-
-  @media screen and (max-width:1230px) {
-.navtag { 
-  font-size: 18px;
-}
-
-.nav {
-  font-size: 18px;   
-}
-
-.but {
-  font-size: 18px; 
-  gap: 20px;
-}
-
-.link {
-  display: flex;
-  gap: 32px;
-}
-
-.butnav {
-  width: 196px;
-  font-size: 18px;
-}
-.logo img {
-    width: 120px;
-  }
-} 
-
-@media screen and (max-width:998px) {
   .navtag {
-    padding: 10px 0;
-    font-size: 18px;
+    font-size: 20px;
   }
 
-  .nav {   
-    font-size: 18px;  
-    text-align: center; 
+  .nav {
+    font-size: 20px;
+    justify-content: space-evenly;
   }
 
   .but {
-    font-size: 18px;   
-    gap: 12px;      
+    font-size: 20px;
+  }
+
+  .butnav {
+    font-size: 20px;
+  }
+}
+
+@media screen and (max-width:1795px) {
+  .navtag {
+    font-size: 20px;
+  }
+
+  .nav {
+    font-size: 20px;
+    justify-content: space-evenly;
+  }
+
+  .but {
+    font-size: 20px;
+  }
+
+  .link {    
+    justify-content: center;
+    gap: 30px;
+  }
+
+  .butnav {
+    font-size: 20px;
+    width: 280px;
+  }
+  .logo img {
+    width: 140px;
+  }
+}
+
+@media screen and (max-width:1581px) {
+  .navtag {
+    font-size: 19px;
+  }
+
+  .nav {
+    font-size: 19px;
+    justify-content: space-evenly;
+  }
+
+  .but {
+    font-size: 19px;
   }
 
   .link {
     display: flex;
-    gap: 18px;
-    padding-right: 12px;
+    gap: 40px;
   }
 
-  .butnav {   
-    width: 189px;
-    font-size: 18px;
+  .butnav {
+    font-size: 19px;
+    width: 260px;
   }
 
   .logo img {
+    width: 130px;
+  }
+}
+
+@media screen and (max-width:1384px) {
+  .navtag {
+    font-size: 18px;
+  }
+
+  .nav {
+    font-size: 18px;
+  }
+
+  .but {
+    font-size: 18px;
+  }
+
+  .butnav {
+    font-size: 18px;  
+    width: 240px;
+  }
+  .logo img {
+    width: 120px;
+  }
+}
+
+@media screen and (max-width:1314px) {
+  .navtag {
+    font-size: 17px;
+  }
+
+  .nav {
+    font-size: 17px;
+  }
+
+  .but {
+    font-size: 17px;
+    gap: 20px;
+  }
+
+  .link {
+    display: flex;
+    gap: 22px;
+  }
+
+  .butnav {
+    width: 224px;
+    font-size: 17px;
+  }
+  .logo img {
     width: 110px;
+  }
+}
+
+@media screen and (max-width:1233px) {
+  .navtag {
+    font-size: 16px;
+  }
+
+  .nav {
+    display: flex;
+    font-size: 16px;
+  }
+
+  .but {
+    font-size: 16px;
+    gap: 10px;
+  }
+
+  .link {
+    display: flex;
+    gap: 10px;
+  }
+
+  .butnav {
+    width: 202px;
+    font-size: 16px;
+  }
+
+  .logo img {
+    width: 96px;
+  }
+}
+
+@media screen and (max-width:998px) {
+  .navtag {
+    padding: 10px 0;
+    font-size: 15px;
+  }
+
+  .nav {
+    font-size: 15px;
+    text-align: center;
+  }
+
+  .but {
+    font-size: 15px;
+    gap: 12px;
+  }
+
+  .link {
+    display: flex;
+    gap: 12px;
+    padding-right: 12px;
+  }
+
+  .butnav {
+    width: 189px;
+    font-size: 15px;
+  }
+
+  .logo img {
+    display: none;
   }
 }
 
 @media screen and (max-width:942px) {
   .navtag {
     padding: 10px 0;
-    font-size: 16px;
+    font-size: 15px;
   }
 
-  .nav {   
-    font-size: 16px;  
-    text-align: center; 
+  .nav {
+    font-size: 15px;
+    text-align: center;
   }
 
   .but {
-    font-size: 16px;   
-    gap: 12px;    
+    font-size: 15px;
+    gap: 10px;
   }
 
   .link {
     display: flex;
-    gap: 18px;
+    gap: 10px;
     padding-right: 12px;
   }
 
-  .butnav {   
-    width: 180px;
-    font-size: 16px;
+  .butnav {
+    width: 190px;
+    font-size: 15px;
   }
 
   .logo img {
@@ -248,51 +314,72 @@ const toggleMenu = () => {
   }
 }
 
-@media screen and (max-width:768px) {
-  .burger {
-    display: flex;   
-  }
-
-  .navtag { 
-    padding: 15px 0; 
-    font-size: 16px;
-    height: 80px;
+@media screen and (max-width:828px) {
+  .navtag {
+    padding: 10px 0;
+    font-size: 14px;
   }
 
   .nav {
-    font-size: 16px;   
-  }
-
-  .link {
-    display: none;
-    flex-direction: column;
-    gap: 10px;
-    position: absolute;
-    padding-bottom: 10px;
-    top: 44px;
-    left: 0;
-    width: 100%;
-    background-color: rgb(30, 33, 61);  
-  }
-
-  .link.active {
-    display: flex;
+    font-size: 14px;
+    text-align: center;
   }
 
   .but {
-    display: none;
+    font-size: 14px;
+    gap: 10px;
   }
 
-  .logo {
+  .link {
+    display: flex;
+    gap: 10px;
+    padding-right: 12px;
+  }
+
+  .butnav {
+    width: 177px;
+    font-size: 14px;
+  }
+
+  .logo img {
     display: none;
   }
 }
 
-/* @media screen and (max-width:576px) {
-  .burger {
-    display: flex;
+@media screen and (max-width:829px) {
+  .navtag {
+    padding: 10px 0;
+    font-size: 14px;
   }
 
+  .nav {
+    font-size: 14px;
+    text-align: center;
+  }
+
+  .but {
+    display: flex;
+    flex-direction: column;
+    font-size: 14px;  
+    text-align: center; 
+  }
+
+  .link {
+    display: flex;  
+    padding: 0 10px;
+  }
+
+  .butnav {
+    width: 178px;
+    font-size: 14px;   
+  }
+
+  .logo img {
+    display: none;
+  }
+}
+
+@media screen and (max-width:576px) {
   .link {
     display: none;
     flex-direction: column;
@@ -315,8 +402,8 @@ const toggleMenu = () => {
 }
   .nav {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
+    flex-direction: column;
+    align-items: flex-start;
     text-decoration: none;
     color: white;
     font-size: 14px;
@@ -330,5 +417,27 @@ const toggleMenu = () => {
   .logo {
     display: none;
   }
-} */
+
+  .burger {
+  display: none;
+  /* flex-direction: column;
+  justify-content: space-around;
+  width: 30px;
+  height: 25px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10; */
+}
+
+.burger span {
+  width: 100%;
+  height: 3px;
+  background: white;
+  transition: all 0.3s linear;
+  position: relative;
+  transform-origin: 1px;
+}
+}
 </style>
