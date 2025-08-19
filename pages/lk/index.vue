8fee1397702586eaf9df9d01c32469ef6bbaa2c1
GenyaSochi <eunasedkina@gmail.com>
2025-08-19 22:03:21 +0300

    _

---
+++ pages/lk/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>LK</p>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import type { project, product, user } from '@prisma/client';
+
+const route = useRoute()
+</script>
--- public/lk/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <p>LK</p>
-</template>
-<script setup lang="ts">
-import {ref} from 'vue'
-// import {users} from 'vue'
-</script>
